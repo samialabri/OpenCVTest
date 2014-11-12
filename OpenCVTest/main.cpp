4a9b87855cc192d797fd9db775bd2b1ee4c86e6c
Sami Al-Abri <Sami@Samis-MacBook-Pro.local>
2014-11-12 11:28:50 +0400

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Sami Al-Abri on 11/12/14.
+//  Copyright (c) 2014 Sami Al-Abri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
